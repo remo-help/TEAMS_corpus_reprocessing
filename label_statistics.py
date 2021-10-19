63ec23ee72768ad8d7552acbbfc30fbd3a31f957
Remo N <61247737+remo-help@users.noreply.github.com>
2021-10-18 21:33:17 -0700

    got it to work as intended, arguments still need to be fleshed out

--- label_statistics.py
+++ label_statistics.py
@@ -8,18 +8,21 @@ def read_in(file,encoding="utf-8"):
    f = open(file, encoding=encoding)
    file = f.read()
    f.close()
-   file = file.split("\n")
+   #file = file.split("\n")
    return file
 
 def find_labels(STRING):
-    pattern1 = re.compile(r'labels":\["([A-Z][a-z]*)",|"labels\\":\[\\"([A-Z][a-z]*)\\",')
+    #pattern1 = re.compile(r'labels":\["([A-Z][a-z]*)",|"labels\\":\[\\"([A-Z][a-z]*)\\",')
+    pattern1 = re.compile(r'labels":\["([A-Z][a-z]*)"',re.IGNORECASE)
    # pattern1= '(\"labels\":[\"([A-Z][a-z]*)\",)|\"labels\\\":[\\\"([A-Z][a-z]*)\\\",'
 
     #for match in pattern1.finditer(STRING):
         #print(match.group(1))
         #print(match.group(2))
-    if re.search(pattern1,STRING):
-        return [match.group(1) for match in pattern1.finditer(STRING)]
+    #if re.findall(pattern1,STRING):
+        #return [match.groups() for match in pattern1.finditer(STRING)]
+
+    return re.findall(pattern1,STRING)
 
 
 
@@ -32,16 +35,37 @@ def main():
     parser.add_argument("--enc",
                         help="The encoding of your extraction file, default = utf-8",
                         required=False)
+    parser.add_argument("--out",
+                        help="The name of the outputfile",
+                        required=False)
     args = parser.parse_args()
     c = Counter()
     if args.enc:
         extraction_file = read_in(args.ext,args.enc)
     else:
         extraction_file = read_in(args.ext)
-        for json in extraction_file:
-            labels = find_labels(str(json))
-            c.update(labels)
+        labels = find_labels(extraction_file)
+        print(labels)
+        #for label in labels:
+            #for i in label:
+                #if i:
+        c.update(labels)
+        #for json in extraction_file:
+        #    labels = find_labels(str(json))
+        #    for label in labels:
+        #        if label:
+        #            c.update(label[0])
         print(c)
+
+    if args.out:
+        pass
+    else:
+        file = open("label_statistics.txt", "w+", encoding='utf-8')
+        for item in c:
+            file.write(item)
+            file.write(":"+str(c[item]))
+            file.write("\n")
+
 
 
         #print(extraction_file[0:5])
